2902d90ce50056718f5336e6ef1f840b6ae66285
dennis githinji <wakahiad@gmail.com>
2024-01-20 12:58:02 +0300

    initial setup

---
+++ src/main/java/org/loopdfs/accountcardservice/AccountCardServiceApplication.java
@@ -0,0 +1,13 @@
+package org.loopdfs.accountcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountCardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/loopdfs/accountcardservice/AccountCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.loopdfs.accountcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountCardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
