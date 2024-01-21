ca1c21a7c874db933b508975fd60b5910ef69c8f
dennis githinji <wakahiad@gmail.com>
2024-01-21 17:34:58 +0300

    setup apidoc config, info and accountController

--- src/main/java/org/loopdfs/accountcardservice/AccountCardServiceApplication.java
+++ src/main/java/org/loopdfs/accountcardservice/AccountCardServiceApplication.java
@@ -1,11 +1,36 @@
 package org.loopdfs.accountcardservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAuditImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loop DFS Account and Cards MicroService REST API Documentation",
+                description = "REST API documentation explain the usage of Account and Card Microservice REST API endpoints",
+                version = "v1",
+                contact = @Contact(
+                        name = "Dennis Githinji",
+                        email = "wakahiad@gmail.com",
+                        url = "https://dennisgithinji.tech"
+                ),
+                license = @License(
+                        name = "Loop DFS 2.0",
+                        url = "https://dennisgithinji.tech"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Loop DFS Account and Cards REST API Documentation",
+                url = "https://dennisgithinji.tech"
+        )
+)
 public class AccountCardServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/loopdfs/accountcardservice/controller/AccountController.java
+++ src/main/java/org/loopdfs/accountcardservice/controller/AccountController.java
@@ -1,7 +1,14 @@
 package org.loopdfs.accountcardservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.loopdfs.accountcardservice.dto.AccountResponseDto;
+import org.loopdfs.accountcardservice.dto.ErrorResponseDto;
 import org.loopdfs.accountcardservice.dto.PaginatedResponse;
 import org.loopdfs.accountcardservice.dto.ResponseDto;
 import org.loopdfs.accountcardservice.service.AccountService;
@@ -10,6 +17,10 @@ import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Account Endpoints",
+        description = "CREATE, UPDATE, DELETE, FETCH AND DELETE endpoints for Account"
+)
 @RestController
 @RequestMapping(value = "/api/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
 @RequiredArgsConstructor
@@ -18,29 +29,177 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API endpoint to create account"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Account created Successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public AccountResponseDto createAccount(@RequestParam("clientId") Long clientId) {
         return accountService.createAccount(clientId);
     }
 
+    @Operation(
+            summary = "Fetch Account REST API",
+            description = "REST API endpoint to fetch account"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ok"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/{accountId}")
     public AccountResponseDto getAccount(@PathVariable("accountId") Long accountId) {
         return accountService.getAccount(accountId);
     }
 
+    @Operation(
+            summary = "Fetch Accounts REST API",
+            description = "REST API endpoint to fetch all accounts"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ok"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @GetMapping
     public PaginatedResponse<AccountResponseDto> getAccounts(@RequestParam(name = "page", defaultValue = "0") int page,
                                                              @RequestParam(name = "size", defaultValue = "10") int size) {
         return accountService.getAllAccounts(page, size);
     }
 
+    @Operation(
+            summary = "Update Account REST API",
+            description = "REST API endpoint to update account"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ok"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @PutMapping("/{accountId}")
     public AccountResponseDto updateAccount(@PathVariable("accountId") Long accountId,
                                             @RequestParam("clientId") Long clientId) {
         return accountService.updateAccount(accountId, clientId);
     }
 
+    @Operation(
+            summary = "Delete Account REST API",
+            description = "REST API endpoint to delete account"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ok"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Resource Not Found",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
     @DeleteMapping("/{accountId}")
     public ResponseDto deleteAccount(@PathVariable("accountId") Long accountId) {
         accountService.deleteAccount(accountId);
